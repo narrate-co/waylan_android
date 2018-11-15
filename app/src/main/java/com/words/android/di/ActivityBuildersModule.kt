d0fa005cb10c0a7537119fdc392b6dfe87b2e2bd
hunterrobbert <hunterstich@google.com>
2018-11-15 09:17:09 -0800

    added BaseUserActivity to Dagger ActivityModule in effort to avoid DispatchingAndroidInjector crash

--- app/src/main/java/com/words/android/di/ActivityBuildersModule.kt
+++ app/src/main/java/com/words/android/di/ActivityBuildersModule.kt
@@ -1,12 +1,17 @@
 package com.words.android.di
 
 import com.words.android.MainActivity
+import com.words.android.ui.common.BaseUserActivity
 import com.words.android.ui.settings.SettingsActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuildersModule {
+
+    @ActivityScope
+    @ContributesAndroidInjector
+    abstract fun contributeBaseUserActivity(): BaseUserActivity
 
     @ActivityScope
     @ContributesAndroidInjector(modules = [MainFragmentBuildersModule::class])
